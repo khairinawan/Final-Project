8a3c1c7097c0991adf504b3dcc0c5562cc128a62
khairinawan <khairinaawan@gmail.com>
2024-11-22 14:36:29 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System | Designed by : WAN NUR KHAIRINA </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
